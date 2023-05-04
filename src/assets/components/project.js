a9c83f215d73a8d2b1faf75b93434c924e13236a
Ari Budiman <rhyman18@gmail.com>
2023-05-04 16:11:22 +0700

    fix(project): delay animation

--- src/assets/components/project.js
+++ src/assets/components/project.js
@@ -2,7 +2,6 @@ import {project} from '/src/assets/data/project';
 
 // Post Project
 const containerProject = document.getElementById('post');
-let j = 1;
 
 const dataProject = await project();
 
@@ -22,7 +21,7 @@ dataProject.data.forEach(function(project, i) {
   createProject.target = '_blank';
   createProject.classList = (i % 2 === 0) ? 'left-bg md:border-e-2 border-main-500' : 'right-bg md:border-s-2 border-main-500';
   createProject.innerHTML = `
-  <article data-aos="zoom-in-${(i % 2 === 0) ? 'right' : 'left'}" data-aos-delay="${j * 100}" class="flex flex-col ${(i % 2 === 0) ? 'md:me-4 lg:me-8 xl:flex-row xl:pe-6' : 'xl:flex-row-reverse md:ms-4 lg:ms-8 xl:ps-6'} text-left items-stretch xl:gap-5 content-box border dark:border-gray-600 dark:hover:border-main-300 rounded dark:shadow-main-500 bg-primary dark:bg-secondary-com">
+  <article data-aos="zoom-in-${(i % 2 === 0) ? 'right' : 'left'}" data-aos-delay="${i * 100}" class="flex flex-col ${(i % 2 === 0) ? 'md:me-4 lg:me-8 xl:flex-row xl:pe-6' : 'xl:flex-row-reverse md:ms-4 lg:ms-8 xl:ps-6'} text-left items-stretch xl:gap-5 content-box border dark:border-gray-600 dark:hover:border-main-300 rounded dark:shadow-main-500 bg-primary dark:bg-secondary-com">
     <div class="overflow-hidden xl:basis-1/3 h-60 md:h-96 xl:h-auto">
       <div class="img-project-zoom slide-img w-full h-full flex justify-center items-center" id="postimg${i}">
         <a href="${project.url}" target="_blank">Live demo</a>
@@ -36,8 +35,6 @@ dataProject.data.forEach(function(project, i) {
     </div>
   </article>
   `;
-
-  j = j + 2;
 
   containerProject.appendChild(createProject);
 
