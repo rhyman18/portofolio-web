9b82995aa18ef3d76b4bc9ad901d3770588eb171
Ari Budiman <rhyman18@gmail.com>
2023-05-14 12:18:03 +0700

    fix(guestbooks): accessible, styling

--- src/scripts/templates/viewGuestbooks.js
+++ src/scripts/templates/viewGuestbooks.js
@@ -5,15 +5,15 @@ const createGuestbook = (guest, link) => `
     <div>
         <div class="flex items-center">
             <i class="fa-brands fa-${guest.platform} fa-3x text-main-500"></i>
-            <h5 class="text-lg font-semibold uppercase ps-2.5">${guest.name}</h5>
+            <h5 class="text-lg font-semibold uppercase ps-2.5" tabindex="0">${guest.name}</h5>
         </div>
-        <div class="pt-2.5 ps-5"><q class="elipsis">${guest.message}</q></div>
+        <div class="pt-2.5 ps-5"><q class="elipsis font-normal" tabindex="0">${guest.message}</q></div>
     </div>
     <div>
-        <div class="text-main-500 font-semibold guest-link my-2.5">
-            <a href="${link + guest.username}" target="_blank">@${guest.username}</a>
+        <div class="text-main-500 font-normal guest-link my-2.5">
+            <a href="${link + guest.username}" target="_blank" class="py-3">@${guest.username}</a>
         </div>
-        <div class="text-main-gray uppercase tracking-widest text-sm">${guest.updated_at}</div>
+        <div class="text-main-gray uppercase tracking-widest text-xs">${guest.updated_at}</div>
     </div>
 </div>
 `;
