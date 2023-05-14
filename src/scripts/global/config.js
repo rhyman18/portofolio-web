02564427096b852b863f72fb43a2ae020a6f830a
Ari Budiman <rhyman18@gmail.com>
2023-05-14 17:51:49 +0700

    fix(location-bg): delete

--- src/scripts/global/config.js
+++ src/scripts/global/config.js
@@ -1,11 +1,11 @@
 const CONFIG = {
-  // BASE_URL: 'https://portfolio.aribudiman.site/api/',
+  BASE_URL: 'https://portfolio.aribudiman.site/api/',
   BASE_IMG_URL: 'https://img.aribudiman.site/',
-  // AUTH: '1|iOE7wCZ9O345C1SckvsCfPXAqm52odNOR6aXIImN',
+  AUTH: '1|iOE7wCZ9O345C1SckvsCfPXAqm52odNOR6aXIImN',
 
   // environment developers
-  BASE_URL: 'http://127.0.0.1:8000/api/',
-  AUTH: '1|nF9MKd6wYbauLV5JrRtSFJLuEPEWMmFFlO0Hd7Pq',
+  // BASE_URL: 'http://127.0.0.1:8000/api/',
+  // AUTH: '1|nF9MKd6wYbauLV5JrRtSFJLuEPEWMmFFlO0Hd7Pq',
 };
 
 export default CONFIG;
