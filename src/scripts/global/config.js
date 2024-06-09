1902f3063cc564fe759c90d180b12014bad6eaee
rhyman18 <rhyman18@gmail.com>
2024-06-09 22:57:15 +0700

    feat: continous integration & setup env config

--- src/scripts/global/config.js
+++ src/scripts/global/config.js
@@ -1,12 +1,10 @@
-const CONFIG = {
-  BASE_URL: 'https://portfolio-api.aribudiman.site/production/',
-  BASE_IMG_URL: 'https://img.aribudiman.site/',
-  AUTH: ' pcqwFh3MRX5nPK0dlF02ONIMZdyECjh9GsM0fqt1',
-  CACHE_NAME: 'aribudiman-site',
+require('dotenv').config();
 
-  // environment developers
-  // BASE_URL: 'http://127.0.0.1:8000/api/',
-  // AUTH: '1|nF9MKd6wYbauLV5JrRtSFJLuEPEWMmFFlO0Hd7Pq',
+const CONFIG = {
+  BASE_URL: process.env.BASE_API_URL,
+  BASE_IMG_URL: process.env.BASE_IMG_URL,
+  AUTH: process.env.AUTH,
+  CACHE_NAME: process.env.CACHE_NAME,
 };
 
 export default CONFIG;
