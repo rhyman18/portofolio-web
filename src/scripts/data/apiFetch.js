92625c76f7f3d5dab78ce79df8b2f300ed9dbbf9
rhyman18 <rhyman18@gmail.com>
2024-07-12 23:35:25 +0700

    fix: api fetch error handle

--- src/scripts/data/apiFetch.js
+++ src/scripts/data/apiFetch.js
@@ -18,27 +18,50 @@ class ApiFetch {
    * @return {Promise} response api
    */
   static async getSkills(section) {
-    const response = await fetch(API_ENDPOINT.SKILLS(section), this.#headers);
-    const results = response.json();
-    return results;
+    try {
+      const response = await fetch(API_ENDPOINT.SKILLS(section), this.#headers);
+      if (!response.ok) {
+        throw new Error('An error occurred while loading the skills data');
+      }
+      const results = await response.json();
+      return results;
+    } catch (error) {
+      console.log('Failed to fetch Api', error);
+      throw new Error('An error occurred while loading the skills data');
+    }
   }
 
   /**
    * get API projects (all)
    */
   static async getProjects() {
-    const response = await fetch(API_ENDPOINT.PROJECTS, this.#headers);
-    const results = response.json();
-    return results;
+    try {
+      const response = await fetch(API_ENDPOINT.PROJECTS, this.#headers);
+      if (!response.ok) {
+        throw new Error('An error occurred while loading the projects data');
+      }
+      const results = await response.json();
+      return results;
+    } catch (error) {
+      console.log('Failed to fetch Api', error);
+      throw new Error('An error occurred while loading the projects data');
+    }
   }
 
   /**
    * get API guestbooks (all)
    */
   static async getGuestbooks() {
-    const response = await fetch(API_ENDPOINT.GUESTBOOKS, this.#headers);
-    const results = response.json();
-    return results;
+    try {
+      const response = await fetch(API_ENDPOINT.GUESTBOOKS, this.#headers);
+      if (!response.ok) {
+        throw new Error('An error occurred while loading the guestbook data');
+      }
+      const results = await response.json();
+      return results;
+    } catch (error) {
+      throw new Error('An error occurred while loading the guestbook data');
+    }
   }
 
   /**
