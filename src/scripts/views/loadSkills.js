420a745c3103e6c488c2fc3a0882d171e74384d9
rhyman18 <rhyman18@gmail.com>
2024-07-12 17:46:18 +0700

    feat(loadSkills): add error handle message & optimize skills

--- src/scripts/views/loadSkills.js
+++ src/scripts/views/loadSkills.js
@@ -1,4 +1,5 @@
 import ApiFetch from '../data/apiFetch';
+import ShowError from '../utils/showError';
 import {createSkeletonSkill, createSkill} from '../templates/viewSkills';
 
 const LoadSkills = {
@@ -19,11 +20,20 @@ const LoadSkills = {
   async _renderSkill(section, container) {
     container.innerHTML = createSkeletonSkill();
 
-    const apiBasic = await ApiFetch.getSkills(section);
-    if (apiBasic.data.length > 0) {
-      container.innerHTML = '';
-      apiBasic.data.forEach((skill) => {
-        container.innerHTML += createSkill(skill);
+    try {
+      const apiBasic = await ApiFetch.getSkills(section);
+      if (apiBasic?.data?.length > 0) {
+        const skillsHTML = apiBasic.data.map((skill) => createSkill(skill)).join('');
+        container.innerHTML = skillsHTML;
+      } else {
+        throw new Error('An error occurred while loading the skills data');
+      }
+    } catch (error) {
+      ShowError.init({
+        containerAlert: document.querySelector('#alert-body'),
+        bodyAlert: document.querySelector('#alert-msg'),
+        messageAlert: `${error}. please <a onclick="window.location.reload()" class="font-semibold underline hover:no-underline cursor-pointer">reload</a> the page.`,
+        alertPriority: 2,
       });
     }
   },
