e0596999665e2d50b21253b1c5250baea6c5547e
rhyman18 <rhyman18@gmail.com>
2024-07-15 22:36:16 +0700

    fix: handle skeleton lazyload certificate

--- src/scripts/templates/viewSkills.js
+++ src/scripts/templates/viewSkills.js
@@ -26,18 +26,13 @@ const createSkill = (skill) => `
 
 const createTooltip = (skill) => `
 <div id="tooltip-target-${skill.id}" role="tooltip" class="w-3/4 sm:w-1/2 md:3/5 lg:max-w-sm tooltip pointer-events-none p-3 md:p-4 text-sm font-medium transition-opacity duration-300 bg-white border rounded-lg shadow-sm opacity-0 dark:bg-gray-800 dark:border-gray-600">
-${
-  skill.cert_img ?
-    `<div class="flex items-center justify-center w-full mb-2 md:mb-3">
-        <img class="lazyload rounded" data-src="${CONFIG.BASE_IMG_URL + skill.cert_img}" />
-    </div>` :
-    `<div class="flex items-center animate-pulse justify-center h-48 mb-2 md:mb-3 bg-gray-300 rounded dark:bg-gray-700">
-        <svg class="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
+    <div class="flex items-center justify-center mb-2 md:mb-3 bg-gray-300 rounded dark:bg-gray-700">
+        ${skill.cert_img ? `<img class="lazyload rounded" data-src="${CONFIG.BASE_IMG_URL + skill.cert_img}" />` : ''}
+        <svg class="animate-pulse w-10 h-10 my-12 sm:my-20 md:my-24 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
             <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z"/>
             <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z"/>
         </svg>
-    </div>`
-}
+    </div>
     <p class="font-normal text-center text-primary-desc dark:text-secondary-desc truncate ...">${skill.cert_desc}</p>
     <div class="tooltip-arrow" data-popper-arrow></div>
 </div>
